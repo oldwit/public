9ad0d4eddb49d8ccefa07e32361cdea237206151
Witold Krak <witold.krak@gmail.com>
2024-09-26 11:30:03 +0200

    Initial kibana util setup.

---
+++ utils/kibana/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+NAME="kibana.util"
+NETWORK_NAME="util"
+echo -e "\e[32m$NAME run script started.\e[0m" && \
+docker network create $NETWORK_NAME && \
+echo -e "\e[32mNetwork $NETWORK_NAME has been created.\e[0m" || \
+echo -e "\e[33mNetwork $NETWORK_NAME already exists.\e[0m"
+docker run -d \
+    -p 5601:5601 \
+    -e ELASTICSEARCH_HOSTS="http://elasticsearch.util:9200" \
+    --rm \
+    --network=$NETWORK_NAME \
+    --name=$NAME \
+    $NAME || \
+echo -e "\e[31mExecute build.sh script first.\e[0m"
+echo -e "\e[32m$NAME run script finished.\e[0m"
