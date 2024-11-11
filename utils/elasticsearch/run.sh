4457271a64fa970b78d37cd930bd8f1f3d6f3368
Witold Krak <witold.krak@gmail.com>
2024-11-11 23:14:31 +0100

    Local volume removed. Run script adjusted. Build script added.

--- utils/elasticsearch/run.sh
+++ utils/elasticsearch/run.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 NAME="elasticsearch.util"
 NETWORK_NAME="util"
+VOLUME_NAME="elasticsearch"
 echo -e "\e[32m$NAME run script started.\e[0m" && \
 docker network create $NETWORK_NAME && \
 echo -e "\e[32mNetwork $NETWORK_NAME has been created.\e[0m" || \
 echo -e "\e[33mNetwork $NETWORK_NAME already exists.\e[0m"
+docker volume create $VOLUME_NAME && \
+echo -e "\e[32mVolume $VOLUME_NAME has been created.\e[0m" || \
+echo -e "\e[33mVolume $VOLUME_NAME already exists.\e[0m"
 docker run -d \
     -e "discovery.type=single-node" \
     -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
-    -p 9200:9200 \
-    -p 9300:9300 \
-    -v ./data:/usr/share/elasticsearch/data \
+    -p 50010:9200 \
+    -p 50011:9300 \
+    -v $VOLUME_NAME:/usr/share/elasticsearch/data \
     --rm \
     --network=$NETWORK_NAME \
     --name=$NAME \
