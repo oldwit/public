41c167d3d5682e3556228d390a62d82256702403
Witold Krak <witold.krak@gmail.com>
2024-11-11 23:51:23 +0100

    Adjustment after postgres changes.

--- utils/logstash/run.sh
+++ utils/logstash/run.sh
@@ -6,9 +6,9 @@ docker network create $NETWORK_NAME && \
 echo -e "\e[32mNetwork $NETWORK_NAME has been created.\e[0m" || \
 echo -e "\e[33mNetwork $NETWORK_NAME already exists.\e[0m"
 docker run -d \
-    -p 5044:5044 \
-    -p 9600:9600 \
-    -p 8080:8080 \
+    -p 50020:5044 \
+    -p 50021:9600 \
+    -p 50022:8080 \
     -v ./pipeline:/usr/share/logstash/pipeline/ \
     --rm \
     --network=$NETWORK_NAME \
